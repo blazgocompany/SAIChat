a01b1bc5d6d334fb6b90d3cbe6d19eb17cc92718
blazgocompany <96586789+blazgocompany@users.noreply.github.com>
2024-08-27 14:13:29 -0700

    Create aitest.py

---
+++ aitest.py
@@ -0,0 +1,11 @@
+from g4f.client import Client
+
+
+client = Client()
+
+chat_completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "Tell me about france"}],
+)
+
+print(chat_completion.choices[0].message.content or "")
